0a1394ced38fee99d4ee69a3a3e6e592cde1eaa1
Allen <ysk19950809@163.com>
2019-11-13 17:31:50 +0800

    膜拜大佬哈希解法

--- LC1.py
+++ LC1.py
@@ -1,6 +1,11 @@
 # -*- coding:utf-8 -*-
 import random
 
+"""
+大佬牛逼
+https://blog.csdn.net/qq_17550379/article/details/80435039
+"""
+
 nums = [10, 4, 2, 1, 6, 9, 5, 7, 13, 45, 33, 26, 19, 14, 22]
 target = 20
 # nums = [2, 7, 11, 15]
@@ -9,14 +14,17 @@ target = 20
 # target = 6
 
 
-# def twoSum(nums, target):
-#     for x in range(len(nums)):
-#         for y in range(x + 1, len(nums)):
-#             if nums[y] + nums[x] == target:
-#                 print(x, y)
-#                 return [x, y]
-#     return []
+# 暴力破解法
+def twoSum(nums, target):
+    for x in range(len(nums)):
+        for y in range(x + 1, len(nums)):
+            if nums[y] + nums[x] == target:
+                print(x, y)
+                return [x, y]
+    return []
 
+
+# 一次循环，避免空表
 def twoSum(nums, target):
     for x in range(len(nums)):
         y = target - nums[x]
@@ -27,5 +35,18 @@ def twoSum(nums, target):
     return []
 
 
+# 大佬的哈希算法，膜拜膜拜
+def twoSum(nums, target):
+    nums_hash = {}
+    for x in range(len(nums)):
+        y = target - nums[x]
+        if y in nums_hash:
+            s = nums_hash[y]
+            print(x, s)
+            return [x, s]
+        nums_hash[nums[x]] = x
+    return []
+
+
 if __name__ == '__main__':
-        twoSum(nums, target)
+    twoSum(nums, target)
